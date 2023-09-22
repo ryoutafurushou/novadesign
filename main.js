fd0c64c57eac6af0662f602b58bf7cb7bbf24ba4
ryoutafurushou <ryota.-041564@gmail.com>
2023-09-22 13:30:38 +0900

    初回コミット

---
+++ main.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+  autoplay:true, // 自動再生を設定
+  slidesToShow: 3, // 表示するスライド数を設定
+  slidesToScroll: 3, // スクロールするスライド数を設定
+  autoplaySpeed:5000, // スライド切り替えの時間を設定
+  dots:true // インジケーターを表示
+});
+
+
+
+
